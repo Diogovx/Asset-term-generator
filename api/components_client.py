dc065af77e57c9be8ee932e3602367d7e800933f
Diogo Velozo Xavier <diogo.velozo@outlook.com>
2025-06-27 14:41:10 -0300

    :recycle: refactor: Arquivos client da api rafatorados, arquivos de classe em core.models criados

--- api/components_client.py
+++ api/components_client.py
@@ -1,9 +1,15 @@
-from typing import Any
+from core.config_manager import API_COMPONENTS_URL
+from core.models import Component
 
 from .base_api_client import BaseAPIClient
 
 
 class ComponentsClient(BaseAPIClient):
-    def get_asset_components(self) -> dict[str, Any]:
-        """Get asset's components"""
-        return self._get(self.base_url)
+    def __init__(self):
+        super().__init__(base_url=API_COMPONENTS_URL)
+
+    def get_all_components(self) -> list[Component]:
+        """Get all components list"""
+        response_data = self._get(self.base_url)
+
+        return [Component(**row) for row in response_data.get("rows", [])]
--- api/hardware_client.py
+++ api/hardware_client.py
@@ -1,9 +1,16 @@
-from typing import Any
+from core.config_manager import API_HARDWARE_URL
 
 from .base_api_client import BaseAPIClient
 
 
+# TODO Hardware Client API
 class HardwareClient(BaseAPIClient):
-    def get_assets_by_employee(self) -> dict[str, Any]:
-        """Get employee's equipaments"""
-        return self._get(self.base_url)
+    """
+    Cliente para interagir com os endpoints de /hardware da API do Snipe-IT.
+    Atualmente não utilizado no fluxo principal, mas mantido para futuras funcionalidades
+    como busca por asset tag ou serial.
+    """
+
+    def __init__(self):
+        super().__init__(base_url=API_HARDWARE_URL)
+        pass
