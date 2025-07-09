752837fbf514ae1953b5aaf8ba0fb4f8f5bcbdfe
Diogo Velozo Xavier <diogo.velozo@outlook.com>
2025-07-09 11:50:53 -0300

    :sparkles: feat: Document generation changed to docxtpl

--- src/assets_term_generator/core/document_processor.py
+++ src/assets_term_generator/core/document_processor.py
@@ -1,6 +1,7 @@
 import logging
 import webbrowser
 from pathlib import Path
+from docxtpl import DocxTemplate
 
 from docx import Document
 from docx.document import Document as DocumentObject
@@ -13,16 +14,10 @@ from .config_manager import OUTPUT_DIR, TEMPLATE_DIR
 logger = logging.getLogger(__name__)
 
 
-def docx_replace(paragraph: Paragraph, old_text: str, new_text: str) -> None:
-    for run in paragraph.runs:
-        if old_text in run.text:
-            run.text = run.text.replace(old_text, new_text)
-
-
 class DocumentProcessor:
     def __init__(self, config: AppConfig) -> None:
         self.config: AppConfig = config
-        self.document: DocumentObject | None = None
+        self.document: DocxTemplate | None = None
         self.active_template_config: TemplateConfig | None = None
 
     def load_template(self, selected_template: str) -> None:
@@ -39,7 +34,7 @@ class DocumentProcessor:
         try:
             self.active_template_config = self.config.document.templates[selected_template]
             template_path = TEMPLATE_DIR / self.active_template_config.file_name
-            self.document = Document(str(template_path))
+            self.document = DocxTemplate(template_path)
             logger.info(f"Template '{template_path.name}' carregado com sucesso.")
         except KeyError as e:
             raise KeyError(f"Template '{selected_template}' não encontrado na configuração.") from e
@@ -55,84 +50,8 @@ class DocumentProcessor:
             "accessories": selected_asset.accessories,
             "components": selected_asset.components,
         }
+        
         return context
-
-    def _resolve_placeholders(self, context: dict) -> dict:
-        """Uses the context to resolve the final value of each placeholder in the configuration.
-        Returns a dictionary of {'[PLACEHOLDER]': 'final_value'}.
-
-        Args:
-            context (dict): User's context.
-        """
-        if not self.active_template_config:
-            raise RuntimeError("Template não foi carregado. Chame load_template() primeiro.")
-
-        all_placeholders = (
-            self.active_template_config.placeholders + self.config.document.default_placeholders
-        )
-
-        replacements = {}
-        user = context["user"]
-        asset = context["asset"]
-
-        for ph in all_placeholders:
-            source = ph.source
-            value = ""
-            if source.type == "text" and source.path:
-                value = getattr(user, source.path, "")
-            elif source.type == "asset" and source.format:
-                value = source.format.format(
-                    model=asset.model.name, asset_tag=asset.asset_tag, serial=asset.serial or ""
-                )
-            elif source.type in ["accessories", "components"]:
-                item_list = context.get(source.type, [])
-
-                def get_category_name(item: Accessory) -> str:
-                    if not item.category:
-                        return ""
-                    if isinstance(item.category, str):
-                        return item.category
-                    return item.category.name
-
-                found_item = next(
-                    (item for item in item_list if get_category_name(item) == ph.category), None
-                )
-
-                if found_item:
-                    if source.format:
-                        format_context = {
-                            "name": found_item.name or "",
-                            "numero_do_celular": asset.get_custom_field("NUMERO") or "",
-                        }
-                        value = source.format.format(**format_context)
-                    elif source.path:
-                        value = getattr(found_item, source.path, "") or ""
-
-            replacements[ph.name] = str(value)
-
-            if ph.generates_presence_marker:
-                base_name = ph.name.strip("[]").replace("MODEL", "")
-                presence_marker_name = f"[HAS{base_name}]"
-
-                if value:
-                    replacements[presence_marker_name] = ph.presence_marker_value or "X"
-                else:
-                    replacements[presence_marker_name] = ""
-        return replacements
-
-    def _replace_in_paragraph(self, paragraph: Paragraph, key: str, value: str) -> None:
-        """Replace markers while maintaining formatting
-
-        Args:
-            paragraph: Document paragraph
-            marker: Marker to be replaced (ex: [NAME])
-            value: Replacement value
-        """
-        if key in paragraph.text:
-            text = paragraph.text.replace(key, value)
-            for run in paragraph.runs:
-                run.text = ""
-            paragraph.runs[0].text = text
 
     def process_document(self, user: User, selected_asset: Asset) -> None:
         """Process asset list and update document
@@ -147,12 +66,9 @@ class DocumentProcessor:
 
         context = self._prepare_context(user, selected_asset)
 
-        replacements = self._resolve_placeholders(context)
+        self.document.render(context)
 
         logger.info("Aplicando substituições no documento...")
-        for paragraph in self.document.paragraphs:
-            for placeholder, value in replacements.items():
-                docx_replace(paragraph, placeholder, value)
 
     def save(self, username: str, asset_tag: str, type_of_term: str) -> Path:
         """Saves the processed document.
@@ -173,8 +89,11 @@ class DocumentProcessor:
             filename = f"{asset_tag} - Termo {type_of_term} - {safe_username}.docx"
 
             output_path = Path(OUTPUT_DIR) / filename
+            OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
             self.document.save(str(output_path))
+            
             logger.info(f"Termo de responsabilidade do usuário {username} criado!")
+            
             return output_path
         except Exception as e:
             logger.error(f"Erro salvando o documento: {e}")
